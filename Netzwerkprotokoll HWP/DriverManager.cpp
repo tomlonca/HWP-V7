12c80203ccf0da917942c84b1204f3c30a7bf991
{tomlonca} <{loncaricatomas@gmail.com}>
2025-01-16 08:10:20 +0100

    Refactor output: change std::cout to std::cerr for error messages in DriverManager class

--- Netzwerkprotokoll HWP/DriverManager.cpp
+++ Netzwerkprotokoll HWP/DriverManager.cpp
@@ -6,9 +6,9 @@ DriverManager::DriverManager(B15F &drv, bool isWriter) : drv(drv) {
     drv.setRegister(&DDRA, 0x0F); //left 4 pins input, 4 right pins output
 
     if(isWriter) {
-        std::cout << "Driver Manager > Writer Instance created!" << std::endl;
+        std::cerr << "Driver Manager > Writer Instance created!" << std::endl;
     } else {
-        std::cout << "Driver Manager > Reciever Instance created!" << std::endl;
+        std::cerr << "Driver Manager > Reciever Instance created!" << std::endl;
     }
     
 }
@@ -27,13 +27,13 @@ void DriverManager::SendData(uint8_t data) {
 void DriverManager::SendFlag(const uint8_t FLAG) {
     
     bool ACK_Recieved = false;
-    std::cout << "DriverManager > Sending Flag (" << static_cast<int>(FLAG) << ")" << std::endl;
+    std::cerr << "DriverManager > Sending Flag (" << static_cast<int>(FLAG) << ")" << std::endl;
 
     while (!ACK_Recieved) {
         drv.setRegister(&PORTA, FLAG);
         /* drv.delay_ms(10);
         drv.setRegister(&PINA, 0x00); */
-        std::cout << "DriverManager > Waiting for ACK..." << std::endl;
+        std::cerr << "DriverManager > Waiting for ACK..." << std::endl;
         if (ReadData() == ACK)
             ACK_Recieved = true;
     }
@@ -44,7 +44,7 @@ uint8_t DriverManager::ReadData() {
     uint8_t Bits_DRV = drv.getRegister(&PINA);
     drv.reverse(Bits_DRV);
     uint8_t inverted_Bits = Bits_DRV  & 0x0F; //IMPORTANT FOR MIRRORED DATA !!!
-    std::cout << "DriverManager > Recieved Data: " << std::bitset<8>(inverted_Bits) << std::endl;
+    std::cerr << "DriverManager > Recieved Data: " << std::bitset<8>(inverted_Bits) << std::endl;
 
     return inverted_Bits; //invert bits so they are not mirrored
 }
