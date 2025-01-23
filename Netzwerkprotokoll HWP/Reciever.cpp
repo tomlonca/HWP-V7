db59fa682b04851c29ca44cc5dcf6b3e531b0640
{tomlonca} <{loncaricatomas@gmail.com}>
2025-01-23 17:37:06 +0100

    korrektur

--- Netzwerkprotokoll HWP/Reciever.cpp
+++ Netzwerkprotokoll HWP/Reciever.cpp
@@ -92,7 +92,7 @@ void Reciever::GetData(bool &isFinished) {
         uint8_t UBits = drvm.ReadData();
         uint8_t LBits = drvm.ReadData();
         std::cerr << "Upper: " << std::bitset<8>(UBits) << " , Lower: " << std::bitset<8>(LBits) << std::endl;
-        LBits = Invert(LBits);
+        Invert(LBits);
         std::cerr << "Lower inverted: " << std::bitset<8>(LBits) << std::endl;
         std::cerr << "Shifted Lower: " << std::bitset<8>(LBits >> 4) << std::endl;
 
@@ -151,5 +151,5 @@ uint8_t Reciever::CalculateCRC8(const std::vector<uint8_t>& data) {
 }
 
 void Reciever::Invert(uint8_t &value) {
-    *value = ~(*value);
+    value = ~value;
 }
